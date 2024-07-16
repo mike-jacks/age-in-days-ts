4bd723deb423a222991805675ff47bd3b5a7acbf
Mike Jacks <mjacks4816@xmail.dixietech.edu>
2024-07-16 11:24:32 -0600

    initialize and insert Calculator component, update css font size to default 62.5%

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import "./App.css";
+import Calculator from "./Components/Calculator/Calculator";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <header>
+        <h1>Age in Days Calculator</h1>
+        <p>Given how many years old someone is, this app will tell you how many days old they are.</p>
+      </header>
+      <main>
+        <Calculator />
+      </main>
+    </>
+  );
 }
 
 export default App;
---
+++ src/Components/Calculator/Calculator.tsx
@@ -0,0 +1,3 @@
+export default function Calculator(): JSX.Element {
+  return <></>;
+}
